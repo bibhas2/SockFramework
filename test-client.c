9308ee9bd2895ca45e40c7f67b662e2b3cef75e4
Bibhas Bhattacharya <bibhas2@gmail.com>
2016-02-06 16:53:04 -0500

    Use convenient method to do an asynchronous connection.

--- test-client.c
+++ test-client.c
@@ -29,7 +29,7 @@ static void onReadable(SocketRec *rec) {
 
 		return;
 	}
-	//printf("%.*s", len, buff);
+	printf("%.*s", len, buff);
 }
 
 static void onWritable(SocketRec *rec) {
@@ -54,42 +54,6 @@ static void onTimeout(SocketRec *rec) {
 	pumpStop(rec->pump);
 }
 
-int make_connection(const char *host, const char *port) {
-	_info("Connecting to %s:%s", host, port);
-
-	struct addrinfo hints, *res;
-
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_family = PF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	_info("Resolving name...");
-	int status = getaddrinfo(host, port, &hints, &res);
-	DIE(status, "Failed to resolve address.");
-	if (res == NULL) {
-		_info("Failed to resolve address: %s", host);
-		abort();
-	}
-
-	int sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	DIE(sock, "Failed to open socket.");
-
-	status = fcntl(sock, F_SETFL, O_NONBLOCK);
-	DIE(status, "Failed to set non blocking mode for socket.");
-
-	status = connect(sock, res->ai_addr, res->ai_addrlen);
-	freeaddrinfo(res);
-
-	_info("Asynchronous connection initiated.");
-	if (status < 0 && errno != EINPROGRESS) {
-		perror("Failed to connect to port.");
-		close(sock);
-
-		return -1;
-	}
-
-	return sock;
-}
-
 int main(int argc, char **argv) {
 	if (argc < 4) {
 		puts("Usage: test_client host port path");
@@ -100,16 +64,15 @@ int main(int argc, char **argv) {
 	const char *port = argv[2];
 	const char *path = argv[3];
 
-        char *fmt = "GET %s HTTP/1.1\r\n"
-                "Host: %s:%s\r\n"
-                "Accept: */*\r\n"
-                "\r\n";
+  char *fmt = "GET %s HTTP/1.1\r\n"
+          "Host: %s:%s\r\n"
+          "Accept: */*\r\n"
+          "\r\n";
 	snprintf(req, sizeof(req), fmt, path, host, port);
 
 	EventPump *pump = newEventPump();
-	int sock = make_connection(host, port);
-	SocketRec *rec = pumpRegisterSocket(pump,
-		sock, req);
+	SocketRec *rec = pumpRegisterClient(pump,
+		host, port, req);
 	rec->onConnect = onConnect;
 	rec->onReadable = onReadable;
 	rec->onWritable = NULL;
