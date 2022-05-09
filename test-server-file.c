b121bb0d61d942dd3db8dfd7cf23411d60989b82
Bibhas Bhattacharya <bibhas2@gmail.com>
2022-05-09 11:34:19 -0400

    Simplify HTTP server code and make it more robust

--- test-server-file.c
+++ test-server-file.c
@@ -8,55 +8,54 @@
 #include "socket-framework.h"
 
 #define _info printf
+#define HTTP_PORT 9090
 
 typedef enum {
 	STATE_NONE,
-	STATE_READING_PROTOCOL_LINE,
-	STATE_PROTOCOL_DONE,
-	STATE_READING_HEADER,
-	STATE_HEADER_DONE,
-	STATE_READING_BODY,
-	HEADER_READ_COMPLETED,
-	WRITING_RESPONSE_HEADER,
-	WRITING_RESPONSE_BODY,
-	RESPONSE_COMPLETED
+	STATE_READ_HEADER,
+	WRITE_RESPONSE_HEADER,
+	WRITE_RESPONSE_BODY
 } ParseState;
 
 typedef struct _HTTPState {
 	ParseState parse_state;
-	char buffer[1024];
-	char io_buffer[1024];
-	int position;
+	char verb[128];
+	char file_name[1024];
+	char write_buffer[1024];
+	char read_buffer[1024];
 	FILE *file;
 } HTTPState;
 
 void
 init_server(Server* state) {
-	_info("Server loop is starting");
+	_info("Server listening on %d.\n", HTTP_PORT);
 }
 
 void on_connect(Server *state, Client *cli_state) {
-	_info("Client connected %d", cli_state->fd);
+	_info("Client connected %d\n", cli_state->fd);
 	HTTPState *httpState = (HTTPState*) malloc(sizeof(HTTPState));
-	httpState->parse_state = STATE_READING_PROTOCOL_LINE;
-	httpState->position = 0;
+
 	httpState->file = NULL;
 
 	cli_state->data = httpState;
 
-	//Start reading request
-	int status = clientScheduleRead(cli_state, httpState->io_buffer, 
-		sizeof(httpState->io_buffer));
+	//Start reading protocol line
+	httpState->parse_state = STATE_READ_HEADER;
+	memset(httpState->read_buffer, '\0', sizeof(httpState->read_buffer));
+	int status = clientScheduleRead(cli_state, httpState->read_buffer, 
+		sizeof(httpState->read_buffer));
+
 	assert(status == 0);
 }
 
 void on_disconnect(Server *state, Client *cli_state) {
-	_info("Client disconnected %d", cli_state->fd);
+	_info("Client disconnected %d\n", cli_state->fd);
 
 	HTTPState *httpState = (HTTPState*) cli_state->data;
 
 	if (httpState->file != NULL) {
 		fclose(httpState->file);
+
 		httpState->file = NULL;
 	}
 
@@ -64,83 +63,86 @@ void on_disconnect(Server *state, Client *cli_state) {
 }
 
 void
-stuff_request_byte(HTTPState *state, char c) {
-	if (state->parse_state == STATE_READING_PROTOCOL_LINE) {
-		if (c == '\r') {
-		} else if (c == '\n') {
-			state->buffer[state->position] = '\0';
-			_info("Protocol line [%s]", state->buffer);
-			state->parse_state = STATE_PROTOCOL_DONE;
-			state->position = 0;
-		} else {
-			state->buffer[state->position] = c;
-			state->position += 1;
-		}	
-	} else if (state->parse_state == STATE_READING_HEADER) {
-		if (c == '\r') {
-		} else if (c == '\n') {
-			state->buffer[state->position] = '\0';
-			_info("Header line [%s]", state->buffer);
-			if (state->position == 0) {
-				state->parse_state = HEADER_READ_COMPLETED;
-			} else {
-				state->parse_state = STATE_HEADER_DONE;
-			}
-			state->position = 0;
-		} else {
-			state->buffer[state->position] = c;
-			state->position += 1;
-		}	
-	}
-}
+write_to_client(HTTPState *http, Client *cli_state, char* line) {
+	char *end = stpncpy(http->write_buffer, line, sizeof(http->write_buffer));
 
-
-void
-start_response(Client *cli_state) {
-	struct stat st;
-
-	int status = stat("image.png", &st);
-	assert(status == 0);
-
-	char str[1024];
-	sprintf(str, "HTTP/1.1 200 OK\r\nContent-Length: %lld\r\n\r\n",
-		st.st_size);
-	
-	HTTPState *httpState = (HTTPState*) cli_state->data;
-	httpState->parse_state = WRITING_RESPONSE_HEADER;
-	clientScheduleWrite(cli_state, str, strlen(str));
+	clientScheduleWrite(cli_state, http->write_buffer, end - http->write_buffer);
 }
 
 void 
-transfer_file_data(Client *cli_state) {
+transfer_file_data(Server *state, Client *cli_state) {
 	HTTPState *httpState = (HTTPState*) cli_state->data;
-	int length = fread(httpState->io_buffer, 1, 
-		sizeof(httpState->io_buffer), httpState->file);
-	if (length > 0) {
-		httpState->parse_state = WRITING_RESPONSE_BODY;
-		clientScheduleWrite(cli_state, httpState->io_buffer, length);
-	}
 
-	if (length < sizeof(httpState->io_buffer)) {
-		//We are done reading
-		httpState->parse_state = RESPONSE_COMPLETED;
+	assert(httpState->file != NULL);
+	assert(httpState->parse_state == WRITE_RESPONSE_BODY);
+
+	int length = fread(httpState->write_buffer, 1, 
+		sizeof(httpState->write_buffer), httpState->file);
+
+	if (length > 0) {
+		clientScheduleWrite(cli_state, httpState->write_buffer, length);
+	} else if (feof(httpState->file)) {
+		//We are done writing
+		fclose(httpState->file);
+		httpState->file = NULL;
+
+		//Allow the client to send another request.
+		httpState->parse_state = STATE_READ_HEADER;
+	} else {
+		perror("File read failed.");
+		serverDisconnect(state, cli_state);
 	}
 }
 
-void on_read(Server *state, Client *cli_state, char *buff, int length) {
+void on_read(Server *state, Client *cli_state, char *buff, size_t length) {
 	HTTPState *httpState = (HTTPState*) cli_state->data;
 
-	for (int i = 0; i < length; ++i) {
-		stuff_request_byte(httpState, buff[i]);
+	if (httpState->parse_state == STATE_READ_HEADER) {
+		//See if we got the verb and file path.
+		//Protocol line will have two spaces: GET /index.php HTTP/1.1
+		int space_count = 0;
 
-		if (httpState->parse_state == STATE_PROTOCOL_DONE) {
-			httpState->parse_state = STATE_READING_HEADER;
-		} else if (httpState->parse_state == STATE_HEADER_DONE) {
-			httpState->parse_state = STATE_READING_HEADER;
-		} else if (httpState->parse_state == HEADER_READ_COMPLETED) {
-			//Send response
-			clientCancelRead(cli_state);
-			start_response(cli_state);
+		for (size_t i = 0; i < cli_state->read_completed; ++i) {
+			if (httpState->read_buffer[i] == ' ') {
+				space_count += 1;
+			}
+
+			if (space_count == 2) {
+				char tmp[1024];
+				
+				sscanf(httpState->read_buffer, "%s %s", httpState->verb, tmp);
+				sprintf(httpState->file_name, ".%s", tmp);
+
+				clientCancelRead(cli_state);
+
+				_info("Request verb: %s path: %s\n", httpState->verb, httpState->file_name);
+
+				httpState->file = fopen(httpState->file_name, "r");
+				httpState->parse_state = WRITE_RESPONSE_HEADER;
+
+				if (httpState->file == NULL) {
+					write_to_client(httpState, cli_state, "HTTP/1.1 404 Not Found\r\n\r\n");
+				} else {
+					struct stat st;
+
+					int status = stat(httpState->file_name, &st);
+
+					if (status == 0) {
+						sprintf(tmp, "HTTP/1.1 200 OK\r\nContent-Length: %lld\r\n\r\n", st.st_size);
+					} else {
+						perror("Can not stat file.");
+
+						strcpy(tmp, "HTTP/1.1 500 Internal Server Error");
+
+						fclose(httpState->file);
+						httpState->file = NULL;
+					}
+
+					write_to_client(httpState, cli_state, tmp);
+				}
+
+				break;
+			}
 		}
 	}
 }
@@ -148,31 +150,34 @@ void on_read(Server *state, Client *cli_state, char *buff, int length) {
 void on_write_completed(Server *state, Client *cli_state) {
 	HTTPState *httpState = (HTTPState*) cli_state->data;
 
-	if (httpState->parse_state == WRITING_RESPONSE_HEADER) {
-		httpState->file = fopen("image.png", "r");
-		assert(httpState->file != NULL);
-		transfer_file_data(cli_state);
-	} else if (httpState->parse_state == WRITING_RESPONSE_BODY) {
-		transfer_file_data(cli_state);
-	} else if (httpState->parse_state == RESPONSE_COMPLETED) {
-		_info("Done writing response. Disconnecting...");
-		//We are done. Disconnect.
-		serverDisconnect(state, cli_state);
+	if (httpState->parse_state == WRITE_RESPONSE_HEADER) {
+		if (httpState->file != NULL) {
+			//Initiate file transfer
+			httpState->parse_state = WRITE_RESPONSE_BODY;
+
+			transfer_file_data(state, cli_state);
+		}
+	} else if (httpState->parse_state == WRITE_RESPONSE_BODY) {
+		if (httpState->file != NULL) {
+			//Continue file transfer
+			transfer_file_data(state, cli_state);
+		}
 	}
 }
 
 void on_read_completed(Server *state, Client *cli_state) {
-	//If we are still parsing request, keep reading
 	HTTPState *httpState = (HTTPState*) cli_state->data;
-	if (httpState->parse_state != HEADER_READ_COMPLETED) {
-		int status = clientScheduleRead(cli_state, httpState->io_buffer, 
-			sizeof(httpState->io_buffer));
-		assert(status == 0);
+
+	//If we are still parsing request then request is too long
+	if (httpState->parse_state == STATE_READ_HEADER) {
+		_info("Request larger than %ld\n", sizeof(httpState->read_buffer));
+
+		serverDisconnect(state, cli_state);
 	}
 }
 
 int main() {
-	Server *state = newServer(9090);
+	Server *state = newServer(HTTP_PORT);
 
 	state->on_loop_start = init_server;
 	state->on_client_connect = on_connect;
